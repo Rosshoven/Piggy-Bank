605f79e01a69ecade5f202d9cba9c75ef1c19f14
Michael Ross <102625452+Rosshoven@users.noreply.github.com>
2023-03-03 14:39:36 -0600

    Update home.jsx

---
+++ piggy-bank/src/routes/home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 style={{marginTop: '15%'}}>Welcome to Piggy Bank</h1>
+            <p>H.O.M.E.</p>
+        </>
+        
+    );
+}

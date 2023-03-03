60dfbee7f6cfd0580b68ca4f4021c4e7c6b0f72e
Rosshoven <Michaeledwardross@gmail.com>
2023-03-03 15:37:24 -0600

    Going HO HO HOME

--- piggy-bank/src/routes/home.jsx
+++ piggy-bank/src/routes/home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
         <>
             <h1 style={{marginTop: '15%'}}>Welcome to Piggy Bank</h1>
             <p>H.O.M.E.</p>
+            <h3>I'm going Ho Ho Home</h3>
         </>
         
     );

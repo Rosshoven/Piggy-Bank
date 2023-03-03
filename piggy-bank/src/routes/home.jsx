bccf3b130c38bab406d3bc662e762b8cc3d81cda
Rosshoven <Michaeledwardross@gmail.com>
2023-03-03 16:53:42 -0600

    styling pages

--- piggy-bank/src/routes/home.jsx
+++ piggy-bank/src/routes/home.jsx
@@ -4,11 +4,12 @@ import home_piggy from '../images/home_piggy.jpg';
 export default function Home() {
     return (
         < div style={{backgroundColor: 'lightblue', padding: '1%'}}>
-            <div className="card" style={{marginRight: 'auto', marginLeft: 'auto', marginTop: '10%', border: 'solid 2px green', width: '50%'}}>
-            <h6 style={{marginLeft: '2%', marginTop: '1%'}}><u>Welcome to...</u></h6>
-            <h1 style={{ marginRight: 'auto', marginLeft: 'auto'}}>PIGGY BANK</h1>
-            <img src={home_piggy} className="card-img-top" alt="..." />
-            <div className="card-body">
+            <div className="card" style={{marginRight: 'auto', marginLeft: 'auto', marginTop: '10%', marginBottom: '10%', border: 'solid 4px green', width: '50%'}}>
+            
+            <h6 style={{paddingLeft: '2%', padding: '1%', backgroundColor: 'pink', marginBottom: '-1%'}}><u>Welcome to...</u></h6>
+            <h1 style={{ backgroundColor: 'pink', width: '100%', display: 'flex', justifyContent: 'center', margin: '0% auto', padding: '4%'}}>PIGGY BANK</h1>
+            <img style={{ borderTop: 'solid 2px green', borderBottom: 'solid 2px green', borderRadius: '0%'}} src={home_piggy} className="card-img-top" alt="..." />
+            <div className="card-body" style={{ backgroundColor: 'pink'}}>
                 <h5 className="card-title">Here at Piggy Bank you can feel safe and secure that your money is protected. Only you can feed the piggy, and only you can break the piggy.</h5>
                 {/* <p className="card-text">Here at Piggy Bank you can feel safe and secure that your money is protected. You can feed the piggy, or you can break the piggy.</p> */}
                 <p className="card-text">Create an account to get started!</p>

aeb1ccb9932678e53e5c77420fc943d505239f9a
Michael Ross <102625452+Rosshoven@users.noreply.github.com>
2023-03-03 14:37:03 -0600

    Update index.js

--- piggy-bank/src/index.js
+++ piggy-bank/src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './error-page';
+import AllData from './routes/all-data';
+import CreateAccount from './routes/create-account.jsx';
+import Deposit from './routes/deposit';
+import Home from './routes/home';
+import Root from './routes/root';
+import Withdraw from './routes/withdraw';
+
+
+// starting to set up my routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'create-account',
+        element: <CreateAccount />
+      },
+      {
+        path: 'deposit',
+        element: <Deposit />
+      }, 
+      {
+        path: 'withdraw',
+        element: <Withdraw />
+      },
+      {
+        path: 'all-data',
+        element: <AllData />
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

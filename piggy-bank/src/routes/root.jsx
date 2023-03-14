165e5e99c4a85ef40185ee0b262bbe7cd3878fcf
Rosshoven <Michaeledwardross@gmail.com>
2023-03-14 13:43:12 -0500

    styles to home page

--- piggy-bank/src/routes/root.jsx
+++ piggy-bank/src/routes/root.jsx
@@ -18,13 +18,14 @@ export default function Root() {
 <nav style={{borderBottom: 'solid 1.5px green', position: 'fixed', width: '100%', backgroundColor: '#ffc4cd', fontFamily: 'Forum, cursive', fontSize: '1.5rem'}} className="nav nav-pills nav-justified navbar navbar-expand-lg navbar-dark sticky-top">
   <div className="container-fluid">
     <Link className="navbar-brand " to={'home'} style={{fontFamily: 'Architects Daughter, cursive', fontSize: '2rem', color: 'lightblue', textShadow: '1.5px 1.5px black', display: 'flex', flexDirection: 'column', alignitems: 'center' }}>
-      <h1>Piggy Bank</h1>
+      <h1>PIGGY BANK</h1>
       <img style={{width:'70px', height: '70px', borderRadius: '50%', border: 'solid .5px black', alignSelf: 'center'}} src={Pig_Logo} alt="Logo" />
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarScroll">
+
+    <div className="collapse navbar-collapse" id="navbarScroll" style={{ marginLeft: '1%'}}>
       <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{bsScrollHeight: "100px"}}>
         
         <li className="nav-link">

c184ea4659d385a669e7ee111302e7ac479cf2ce
Rosshoven <Michaeledwardross@gmail.com>
2023-03-16 10:34:02 -0500

    Welcome change

--- piggy-bank/src/routes/home.jsx
+++ piggy-bank/src/routes/home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <h1 style={{ backgroundColor: 'pink', width: '100%', display: 'flex', justifyContent: 'center', margin: '0% auto', padding: '.5%', fontFamily: 'Architects Daughter, cursive', fontSize: '5rem', color: 'lightblue', textShadow: '1.5px 3px 2px black'}}>PIGGY BANK</h1>
             <img style={{ borderTop: 'solid 2px green', borderBottom: 'solid 2px green', borderRadius: '0%'}} src={home_piggy} className="card-img-top" alt="..." />
             <div className="card-body" style={{ backgroundColor: 'pink' }}>
-                <h5 className="card-title" style={{fontSize: '1.4rem'}}>Here at Piggy Bank you can feel safe and secure knowing that your money is protected. Only you can feed the piggy, and only you can break the piggy.</h5>
+                <h5 className="card-title" style={{fontSize: '1.4rem'}}>Here at Piggy Bank you can feel safe and secure knowing that your money is protected by a pig. Only you can feed the piggy, and only you can break the piggy.</h5>
                 {/* <p className="card-text">Here at Piggy Bank you can feel safe and secure that your money is protected. You can feed the piggy, or you can break the piggy.</p> */}
                 <p className="card-text">Create an account to get started!</p>
             </div>

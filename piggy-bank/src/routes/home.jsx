4d19525edc0a35e209365e4331fd7c3cb925d740
Rosshoven <Michaeledwardross@gmail.com>
2023-03-16 22:33:41 -0500

    style to home and nav

--- piggy-bank/src/routes/home.jsx
+++ piggy-bank/src/routes/home.jsx
@@ -2,12 +2,12 @@ import home_piggy from '../images/home_piggy.jpg';
 
 export default function Home() {
     return (
-        < div style={{backgroundColor: 'lightblue', padding: '2%'}}>
-            <div className="card" style={{marginRight: 'auto', marginLeft: 'auto', marginTop: '10%', marginBottom: '10%', border: 'solid 4px green', width: '40%', fontFamily: 'Forum, cursive', fontSize: '1.4rem'}}>
+        < div style={{backgroundColor: 'lightblue', padding: '3%'}}>
+            <div className="card" style={{marginRight: 'auto', marginLeft: 'auto', marginTop: '10%', border: 'solid 4px green', width: '42%', fontFamily: 'Forum, cursive', fontSize: '1.4rem'}}>
             
                 <h6 style={{padding: '2% 2%', backgroundColor: 'pink', marginBottom: '-1%', fontFamily: 'Forum, cursive'}}><u>Welcome to...</u></h6>
                 <h1 style={{ backgroundColor: 'pink', width: '100%', display: 'flex', justifyContent: 'center', margin: '0% auto', padding: '.5%', fontFamily: 'Architects Daughter, cursive', fontSize: '5rem', color: 'lightblue', textShadow: '1.5px 3px 2px black'}}>PIGGY BANK</h1>
-                <img style={{ borderTop: 'solid 2px green', borderBottom: 'solid 2px green', borderRadius: '0%'}} src={home_piggy} className="card-img-top" alt="..." />
+                <img style={{ borderTop: 'solid 2px green', borderBottom: 'solid 2px green', borderRadius: '0%'}} src={home_piggy} className="card-img-top" alt="Piggy Bank on Gold Coins" />
                 
                 <div className="card-body" style={{ backgroundColor: 'pink' }}>
                     <h5 className="card-title" style={{fontSize: '1.4rem', fontFamily: 'Forum, cursive'}}>Here at Piggy Bank you can feel safe and secure knowing that your money is protected by a pig. Only you can feed the piggy, and only you can break the piggy.</h5>
--- piggy-bank/src/routes/root.jsx
+++ piggy-bank/src/routes/root.jsx
@@ -20,13 +20,11 @@ export default function Root() {
       <>
       <BalanceContext.Provider value={{balance, setBalance}}>
         <div>
-
-         {/* Took out 'bg-body-secondary' from <nav className> Added 'navbar-dark bg-dark' ... took out 'bg-dark and replaced with 'pink'*/}
 <nav style={{borderBottom: 'solid 1.5px green', position: 'fixed', width: '100%', backgroundColor: '#ffc4cd', fontFamily: 'Forum, cursive', fontSize: '1.5rem'}} className="nav nav-pills nav-justified navbar navbar-expand-lg navbar-dark sticky-top">
   <div className="container-fluid">
-    <Link className="navbar-brand " to={'home'} style={{fontFamily: 'Architects Daughter, cursive', fontSize: '2rem', color: 'lightblue', textShadow: '1.5px 1.5px black', display: 'flex', flexDirection: 'column', alignitems: 'center' }}>
-      <h1>PIGGY BANK</h1>
-      <img style={{width:'70px', height: '70px', borderRadius: '50%', border: 'solid .5px black', alignSelf: 'center'}} src={Pig_Logo} alt="Logo" />
+    <Link className="navbar-brand " to={'home'} style={{fontFamily: 'Architects Daughter, cursive', fontSize: '2rem', color: 'lightblue', textShadow: '1.5px 1.5px black', display: 'flex', flexDirection: 'row-reverse', alignitems: 'center' }}>
+      <h1 style={{marginLeft: '1.2%'}}>PIGGY BANK</h1>
+      <img style={{width:'70px', height: '70px', borderRadius: '50%', border: 'solid .5px black'}} src={Pig_Logo} alt="Logo" />
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -46,7 +44,7 @@ export default function Root() {
          <NavLink to={'create-account'}  className={({isActive}) => {
           return `nav-link active${isActive ? ' bg-success hover:bg-gray-700 hover:text-white' : 'bg-gray-900 text-white'}`
           }}>Create Account</NavLink>
-          {cAShown && (<div style={{borderRadius: '5px', fontSize: '1rem', color: 'black', backgroundColor: 'antiquewhite', marginTop: '3%', display: 'flex', justifyContent: 'center', padding: '2%'}}>Create your Piggy Bank here!</div>)}
+          {cAShown && (<div style={{borderRadius: '5px', fontSize: '1rem', color: 'black', backgroundColor: 'antiquewhite', marginTop: '3%', display: 'flex', justifyContent: 'center', padding: '2%'}}>Create your Piggy Bank!</div>)}
         </li>
         
         <li className="nav-link" onMouseEnter={() => setDepShown(true)} onMouseLeave={() => setDepShown(false)}>
